9699ff287a522b9ad13d8dacb8f26686d7384a4a
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-10-13 10:40:07 -0500

    Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,30 @@ export default class index extends Component {
     return (
       <div>
         <h1>¡Hola mundo!</h1>
-          <p>Bienvenidos</p>
+        <p>Bienvenidos</p>
+        <img src="/platzi-logo.png" alt="Platzi logo" />
+        <style jsx>
+          {`
+            h1 {
+              color: red;
+            }
+            p {
+              color: green;
+            }
+            img {
+              max-width: 50%;
+              display: block;
+              margin: 0 auto;
+            }
+          `}
+        </style>
+        <style jsx global>
+          {`
+            body {
+              background: white;
+            }
+          `}
+        </style>
       </div>
     )
   }
