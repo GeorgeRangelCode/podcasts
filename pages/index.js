a80d91b6ad3924a20960b92bd791c138938c05bd
GeorgeRangelCode <georgerangelcode@gmail.com>
2020-10-13 11:25:14 -0500

    Intro a getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,69 @@
-import React, { Component } from 'react'
+import "isomorphic-fetch";
 
-export default class index extends Component {
-  render() {
-    return (
-      <div>
-        <h1>¡Hola mundo!</h1>
-        <p>Bienvenidos</p>
-        <img src="/platzi-logo.png" alt="Platzi logo" />
-        <style jsx>
-          {`
-            h1 {
-              color: red;
-            }
-            p {
-              color: green;
-            }
-            img {
-              max-width: 50%;
-              display: block;
-              margin: 0 auto;
-            }
-          `}
-        </style>
-        <style jsx global>
-          {`
-            body {
-              background: white;
-            }
-          `}
-        </style>
+const App = ({ channels }) => {
+  return (
+    <>
+      <header>Podcasts</header>
+
+      <div className="channels">
+        {channels.map((channel, index) => (
+          <div key={`${channel.id}-${index}`} className="channel">
+            <img src={channel.urls.logo_image.original} alt={channel.title} />
+            <h2>{channel.title}</h2>
+          </div>
+        ))}
       </div>
-    )
-  }
+
+      <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+
+        .channel {
+          display: block;
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+          margin-bottom: 0.5em;
+        }
+
+        .channel img {
+          width: 100%;
+        }
+
+        .channel h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          background: white;
+          font-family: system-ui;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  let req = await fetch("https://api.audioboom.com/channels/recommended");
+  let { body: channels } = await req.json();
+  return { props: { channels } };
 }
+
+export default App;
